94f070241b3ca88b8cfc59f8b909369e422f54e4
albayaaan <albayaaan.z@gmail.com>
2023-02-22 09:07:08 +0700

    Day 4 - create relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    function purchaseOrderLine()
+    {
+        return $this->hasMany(PurchaseOrderLine::class);
+    }
 }
